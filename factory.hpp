5d67575404d403506a0744b20b2850b0337f27f9
Armin Schmidt <aschmidt@seppl.localdomain>
2007-09-04 09:37:55 +0200

    Initial commit of factory.hpp.

---
+++ factory.hpp
@@ -0,0 +1,47 @@
+#ifndef _FACTORY_HPP
+#define _FACTORY_HPP
+
+#include <map>
+#include <boost/bind.hpp>
+
+template
+<
+	class AbstractProduct,
+	typename IdentifierType,
+	typename ProductCreator = AbstractProduct* (*)()
+>
+class Factory
+{
+public:
+	bool Register(const IdentifierType& id, ProductCreator creator)
+	{
+		return f_.insert(std::make_pair(id, creator)).second;
+	}
+
+	ProductCreator & CreationFunction(const IdentifierType& id)
+	{
+		typename factory_map_t::iterator it = f_.find(id);
+		if(it != f_.end())
+		{
+			return (it->second);
+		}
+
+	}
+
+	AbstractProduct * CreateObject(const IdentifierType& id)
+	{
+		typename factory_map_t::iterator it = f_.find(id);
+		if(it != f_.end())
+		{
+			return (it->second)();
+		}
+
+	}
+
+private:
+	typedef std::map< IdentifierType, ProductCreator > factory_map_t;
+	
+	factory_map_t f_;
+};
+
+#endif // _FACTORY_HPP
