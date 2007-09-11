8ca1b778e41884d108d4b13b212a90a64362df4a
Armin Schmidt <aschmidt@seppl.localdomain>
2007-09-11 14:15:45 +0200

    [macros] added H_DECLARE_PRIVATE and H_DECLARE_PROTECTED.
    Shamelessly storen from Qt...

--- macros.hpp
+++ macros.hpp
@@ -3,4 +3,13 @@
 
 #define H_D(CLASS) CLASS##Impl * const d = d_func();
 
+#define H_DECLARE_PRIVATE(CLASS)\
+friend class CLASS##Impl;\
+inline CLASS##Impl *d_func() { return d_ptr; }\
+inline const CLASS##Impl *d_func() const { return d_ptr; }
+
+#define H_DECLARE_PROTECTED(CLASS)\
+CLASS##Impl * const d_ptr;\
+CLASS(CLASS##Impl &dd);
+
 #endif // _MACROS_HPP
