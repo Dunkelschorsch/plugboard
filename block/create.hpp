433362bff36ca296919c05a3024b3da71679399b
Armin Schmidt <aschmidt@lnt.de>
2008-01-24 11:17:20 +0100

    [Block] the ACCESS_FUNC macro now uses the BOOST_PP

--- block/create.hpp
+++ block/create.hpp
@@ -1,15 +1,19 @@
 #ifndef BLOCK_CREATE_HPP
 #define BLOCK_CREATE_HPP
 
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+
 #define ACCESS_FUNCS(NAME)	\
 extern "C" {			\
 Block* create()			\
 {				\
-	return new Block_##NAME;\
+	return new BOOST_PP_CAT(Block_, NAME);\
 }				\
 const string_t name()		\
 {				\
-	return #NAME;		\
+	return BOOST_PP_STRINGIZE(NAME);	\
 } }
 
 #endif // BLOCK_CREATE_HPP
