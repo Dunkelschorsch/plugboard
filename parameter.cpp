50ef78fd35dc5cc7c0cf5727e1e86a3e526ed061
Armin Schmidt <aschmidt@lnt.de>
2008-04-11 16:16:36 +0200

    Added copyright and GPL headers and a copy of the GPLv3

--- parameter.cpp
+++ parameter.cpp
@@ -1,3 +1,31 @@
+/*!
+ * \file
+ * \brief
+ * \author Armin Schmidt
+ *
+ * ----------------------------------------------------------------------------
+ *
+ * PlugBoard - A versatile communication simulation framework
+ * Copyright (C) 2007-2008  Armin Schmidt
+ *
+ * This file is part of PlugBoard.
+ *
+ * PlugBoard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PlugBoard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PlugBoard.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ----------------------------------------------------------------------------
+ */
+
 #include "parameter.hpp"
 #include "variable/variable.hpp"
 #include "constraint.hpp"
